3ee7e78d7e0ed113b562101fdbbc877c0c456fce
senai_25_26 <diegobertaglia450@gmail.com>
2026-09-14 14:38:27 -0300

    first commit

--- app.py
+++ app.py
@@ -6,11 +6,17 @@ app.config['SECRET_KEY'] = '12345'
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("pagina_inicial.html")
 
 @app.route('/login')
 def login():
     return render_template("login.html")
+
+
+@app.route('/base2')
+def base2():
+    return render_template("base2.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
